e50372974b4a11c33c7a7fcdb9b35d9e958f947f
SenthilKumar <toskumar@yahoo.com>
2021-09-28 17:28:49 +0530

    java doc updated

--- java8/src/com/example/chapter1/Operators.java
+++ java8/src/com/example/chapter1/Operators.java
@@ -4,6 +4,8 @@ public class Operators {
 
 	public static void main(String args[]) {
 		int a = 0;
+		
+		// Relational operator
 		System.out.println("!true =>"+ !true);
 		System.out.println("true == true => " + (true == true));
 		System.out.println("true != false => " + (true != false));
@@ -12,20 +14,28 @@ public class Operators {
 		System.out.println("10 <= 20 && 20 >=10 => " + (10 <= 20 && 20 >=10));
 		System.out.println("10 <= 20 || 20 >=10 => " + (10 <= 20 || 20 >=10));
 		System.out.println("a=10 => "+(a=10));
+		
+		//Unary operator
 		System.out.println("++a =>"+ ++a);
 		System.out.println("a++ =>"+ a++);
 		System.out.println("a-- =>"+ a--);
 		System.out.println("--a =>"+ --a);
+		
+		//Shift operator
 		System.out.println("128<<1 => "+(128<<1));
 		System.out.println("128>>>1 => "+(128>>1));
-		
+	
+		//Arithmetic Operator
 		int x = 10;
 		int y = 20;
+	
 		System.out.println("x+y => " + (x+y));
 		System.out.println("x-y => " + (x-y));
 		System.out.println("x*y => " + (x*y));
 		System.out.println("x/y => " + (x/y));
+		System.out.println("x%2 => " + (x%2));
 		
+		//Ternary Opertor
 		System.out.println("x>y?x:y => " + (x>y?x:y));
 	}
 }
