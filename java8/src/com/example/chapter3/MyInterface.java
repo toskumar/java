c8a47c2f157e4299ddc27eb1bb281bb847e47b31
SenthilKumar <toskumar@yahoo.com>
2021-10-02 10:34:48 +0530

    java doc updated

--- java8/src/com/example/chapter3/MyInterface.java
+++ java8/src/com/example/chapter3/MyInterface.java
@@ -6,32 +6,37 @@ package com.example.chapter3;
  */
 public class MyInterface {
 
-	MyInterface() {
-	}
 	public static void main(String args[]) {
-		Driver oracleDriver = new OracleDatabase();
-		oracleDriver.connect();
-		
-		Driver mysqlDriver = new MySQLDatabase();
-		mysqlDriver.connect();
+		Wifi wifi1 = new AirtelService();
+		Wifi wifi2 = new JioService();
+
+		Guest guest1 = new Guest(wifi1, "pass1");
+		Guest guest2 = new Guest(wifi2, "pass2");
 	}
 }
 
-interface Driver {
-	static final String PROTOCOL = "JDBC";
-	void connect();
+interface Wifi {
+	static final String PROTOCOL = "wifi";
+	void connect(String password);
 }
 
-class OracleDatabase implements Driver {
+class AirtelService implements Wifi {
 	@Override
-	public void connect() {
-		System.out.println("Conntected to Oracle Database using " + PROTOCOL);
+	public void connect(String password) {
+		System.out.println("Conntected through Airtel service using " + PROTOCOL);
 	}
 }
 
-class MySQLDatabase implements Driver {
+class JioService implements Wifi {
 	@Override
-	public void connect() {
-		System.out.println("Conntected to MySQL Database using " + PROTOCOL);
+	public void connect(String password) {
+		System.out.println("Conntected through Jio service using " + PROTOCOL);
+	}
+}
+
+class Guest {
+	
+	Guest(Wifi wifi, String password) {
+		wifi.connect(password);
 	}
 }
