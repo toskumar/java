5213badec46cc6d74720ba464f2f54836309434c
SenthilKumar <toskumar@yahoo.com>
2022-03-09 20:46:01 +0530

    Simple Thread

---
+++ java8/src/com/example/chapter4/SimpleThread.java
@@ -0,0 +1,23 @@
+package com.example.chapter4;
+
+public class SimpleThread {
+
+	
+	public static void main(String args[])  {
+		
+		Runnable r1 = () -> {
+			for(int i=0;i<10;i++) {
+				System.out.println(Thread.currentThread().getName() + " is running");
+				try {
+					Thread.sleep(400);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		new Thread(r1, "Thread-1").start();
+		new Thread(r1, "Thread-2").start();
+		
+	}
+}
